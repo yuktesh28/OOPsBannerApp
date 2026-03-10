fa638ff690a2ad8f707d69300b62c2392199711d
yukteshwar <yuktesh2007@gmail.com>
2026-03-10 03:03:48 +0000

    Add BannerApp3 class and VSCode settings for Java debugging

---
+++ BannerApp3.java
@@ -0,0 +1,14 @@
+
+public class BannerApp3 {
+	public static void main(String[] args){
+		System.out.println("...");		   
+	    System.out.println(String.join(" ","   ***   " , " ", "   ***   " , " ", " ******  " , " ", "  *****  "));
+		System.out.println(String.join(" "," **   ** " , " ", " **   ** " , " ", " **    **" , " ", " **   ** "));
+		System.out.println(String.join(" ","**     **" , " ", "**     **" , " ", " **    **" , " ", " **      "));
+		System.out.println(String.join(" ","**     **" , " ", "**     **" , " ", " ******  " , " ", "  *****  "));
+		System.out.println(String.join(" ","**     **" , " ", "**     **" , " ", " **      " , " ", "      ** "));
+		System.out.println(String.join(" "," **   ** " , " ", " **   ** " , " ", " **      " , " ", " **   ** "));
+		System.out.println(String.join(" ","   ***   " , " ", "   ***   " , " ", " **      " , " ", "  *****  "));
+    }
+}
+
